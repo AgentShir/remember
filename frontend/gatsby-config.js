b7efde6a3efc5c129fe040c9373771e77c876dc9
Marshall.Tuinier <Mtuinier@gmail.com>
2018-11-17 09:39:31 -0800

    initial commit

---
+++ frontend/gatsby-config.js
@@ -0,0 +1,29 @@
+module.exports = {
+    plugins: [
+      {
+        resolve: `gatsby-plugin-styled-components`,
+        options: {}
+      },
+      {
+        resolve: `gatsby-plugin-typography`,
+        options: {
+          pathToConfigModule: `src/utils/typography.js`
+        }
+      },
+      {
+        resolve: "gatsby-plugin-page-transitions",
+        options: {
+          transitionTime: 625
+        }
+      },
+      {
+        resolve: `gatsby-source-filesystem`,
+        options: {
+          name: `images`,
+          path: `src/assets`
+        }
+      },
+      `gatsby-plugin-sharp`,
+      `gatsby-transformer-sharp`
+    ]
+  };
---
+++ frontend/src/utils/typography.js
@@ -0,0 +1,7 @@
+import Typography from "typography";
+import sternGroveTheme from "typography-theme-stern-grove";
+
+sternGroveTheme.bodyWeigt = 500;
+const typography = new Typography(sternGroveTheme);
+
+export default typography;
